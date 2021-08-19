6edc7bd2161ba50ed133e563fafc39ba885cb7c7
VaishakKS <vaishakmnglr@gmail.com>
2021-08-19 17:35:03 +0000

    linked list final commit

--- DataStructures/LinkedList/SinglyLinkedList.js
+++ DataStructures/LinkedList/SinglyLinkedList.js
@@ -12,10 +12,12 @@ class LinkedListNode {
 class LinkedList {
 
     constructor() {
+        this.length = 0;
         this.head = null;
         this.tail = null;
     }
 
+    //Adds a new value from the begining of linked list
     prepend(value) {
         // Make new node to be a head.
         const newNode = new LinkedListNode(value, this.head);
@@ -26,9 +28,12 @@ class LinkedList {
             this.tail = newNode;
         }
 
+        this.length++;
+
         return this;
     }
 
+    //Adds new values from the end of the linked list
     append(value) {
         const newNode = new LinkedListNode(value);
 
@@ -37,6 +42,7 @@ class LinkedList {
             this.head = newNode;
             this.tail = newNode;
 
+            this.length++;
             return this;
         }
 
@@ -44,69 +50,12 @@ class LinkedList {
         this.tail.next = newNode;
         this.tail = newNode;
 
+        this.length++;
+
         return this;
     }
 
-    // delete(value) {
-    //     if (!this.head) {
-    //         return null;
-    //     }
-
-    //     let deletedNode = null;
-
-    //     // If the head must be deleted then make next node that is different
-    //     // from the head to be a new head.
-    //     while (this.head && this.compare.equal(this.head.value, value)) {
-    //         deletedNode = this.head;
-    //         this.head = this.head.next;
-    //     }
-
-    //     let currentNode = this.head;
-
-    //     if (currentNode !== null) {
-    //         // If next node must be deleted then make next node to be a next next one.
-    //         while (currentNode.next) {
-    //             if (this.compare.equal(currentNode.next.value, value)) {
-    //                 deletedNode = currentNode.next;
-    //                 currentNode.next = currentNode.next.next;
-    //             } else {
-    //                 currentNode = currentNode.next;
-    //             }
-    //         }
-    //     }
-
-    //     // Check if tail must be deleted.
-    //     if (this.compare.equal(this.tail.value, value)) {
-    //         this.tail = currentNode;
-    //     }
-
-    //     return deletedNode;
-    // }
-
-    // find({ value = undefined, callback = undefined }) {
-    //     if (!this.head) {
-    //         return null;
-    //     }
-
-    //     let currentNode = this.head;
-
-    //     while (currentNode) {
-    //         // If callback is specified then try to find node by callback.
-    //         if (callback && callback(currentNode.value)) {
-    //             return currentNode;
-    //         }
-
-    //         // If value is specified then try to compare by value..
-    //         if (value !== undefined && this.compare.equal(currentNode.value, value)) {
-    //             return currentNode;
-    //         }
-
-    //         currentNode = currentNode.next;
-    //     }
-
-    //     return null;
-    // }
-
+    //Removes the last element from linked list
     deleteTail() {
         const deletedTail = this.tail;
 
@@ -114,6 +63,8 @@ class LinkedList {
             // There is only one node in linked list.
             this.head = null;
             this.tail = null;
+
+            this.length--;
 
             return deletedTail;
         }
@@ -131,10 +82,12 @@ class LinkedList {
         }
 
         this.tail = currentNode;
+        this.length--;
 
         return deletedTail;
     }
 
+    //Removes the first element from linked list
     deleteHead() {
         if (!this.head) {
             return null;
@@ -149,15 +102,105 @@ class LinkedList {
             this.tail = null;
         }
 
+        this.length--;
         return deletedHead;
     }
 
-    fromArray(values) {
-        values.forEach((value) => this.append(value));
+    //Adds a value at an index position of linked list
+    addAt(index, element){
+        index--;
+        // Check if index is out of bounds of list
+        if (index > this.length) {
+            console.log('Index out of Bound.');
+            return;
+        }
+        const node = new LinkedListNode(element);
 
-        return this;
+        let currentNode = this.head
+        let previousNode
+        let currentIndex = 0
+    
+        // Check if index is the start of list
+        if (index === 0) {
+          node.next = currentNode
+          this.head = node
+        } else {
+          while (currentIndex < index) {
+            currentIndex++
+            previousNode = currentNode
+            currentNode = currentNode.next
+          }
+    
+          // Adding the node at specified index
+          node.next = currentNode
+          previousNode.next = node
+        }
+    
+        // Incrementing the length
+        this.length++
+        return true
     }
 
+    //Removes a value at an index position of linked list
+    removeAt(index){
+        index--;
+
+        // Check if index is present in list
+        if (index < 0 || index >= this.length) {
+            console.log('index value is invalid');
+            return;
+        }
+        let currentNode = this.head
+        let previousNode
+        let currentIndex = 0
+    
+        // Check if element is the first element
+        if (index === 0) {
+          this.head = currentNode.next
+        } else {
+          while (currentIndex < index) {
+            currentIndex++
+            previousNode = currentNode
+            currentNode = currentNode.next
+          }
+          previousNode.next = currentNode.next
+        }
+    
+        // Decrementing the length
+        this.length--;
+        return currentNode.value;
+    }
+
+    //Returns the element at a particular index position
+    elementAt(index) {
+        let currentNode = this.head
+        let count = 0
+        while (count < index) {
+          count++
+          currentNode = currentNode.next
+        }
+        return currentNode.value;
+    }
+
+    // Returns the index of the element passed as param otherwise -1
+    indexOf(value) {
+        let currentNode = this.head
+        let index = -1
+
+        while (currentNode) {
+        index++
+
+        // Checking if the node is the element we are searching for
+        if (currentNode.value === value) {
+            return index;
+        }
+        currentNode = currentNode.next
+        }
+
+        return -1
+    }
+
+    //Convert the linked list into an array
     toArray() {
         const nodes = [];
 
@@ -170,6 +213,7 @@ class LinkedList {
         return nodes;
     }
 
+    //String reperesentation of linked list
     toString(callback) {
         let list = '';
         this.toArray().forEach((node,index) => 
@@ -184,7 +228,7 @@ class LinkedList {
         return list;
     }
 
-
+    //Reverses the order of the linked list
     reverse() {
         let currNode = this.head;
         let prevNode = null;
@@ -208,21 +252,57 @@ class LinkedList {
 
         return this;
     }
+
+    //Returns the length of the linked list(number of elements in the list)
+    size() {
+        return this.length;
+    }
 }
 
 const linklist = new LinkedList()
+
+//Adds new values 2,3,4,5 from end of the linked list
 linklist.append(2)
 linklist.append(3)
 linklist.append(4)
 linklist.append(5)
 console.log(linklist.toString());
+
+//Adds new values 2,1 from begining of the linked list
 linklist.prepend(2)
 linklist.prepend(1)
 console.log(linklist.toString());
+
+//Removes a value from the begining of linked list
 console.log(`Deleted head: ${linklist.deleteHead()}`);
 console.log(linklist.toString());
+
+//Removes a value from the end of linked list
 console.log(`Deleted Tail: ${linklist.deleteTail()}`);
 console.log(linklist.toString());
-linklist.reverse()
+
+//Reverses the order of the linked list
+linklist.reverse();
 console.log("Revered List:");
 console.log(linklist.toString());
+
+//Returns the length of the linked list
+console.log(linklist.size());
+
+//Adds a new value '5' at position 2(index: 1)
+linklist.addAt(2,5);
+console.log(linklist.toString());
+
+//Adds a new value '5' at position 7(index: 6)
+linklist.addAt(7,5);
+console.log(linklist.toString()); //Expected output: Index out of bound
+
+//Removes a value at position 2(index: 1) i., 5
+console.log('Removed value: '+linklist.removeAt(2));
+console.log(linklist.toString());
+
+//Returns the element at index position 2 i.e, 2
+console.log(linklist.elementAt(1));
+
+// Returns the index of element 3 i.e., index = 1
+console.log(linklist.indexOf(3));
